b6ce5fa8f7b8c488477cfea7fb77074e45910f36
anniemcghee <mcghee.annie@gmail.com>
2014-11-21 12:41:38 -0800

    I made my temp converter work

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
 	var button1 = document.querySelector('.btn');
-	var farInputTemp = parseInt(document.querySelector('.form-control'));
-	var celResult = ((farInputTemp-32)*(5 / 9)).toFixed(0);
+	
+	var button2 = document.querySelector('#refresh')
+
+	button2.addEventListener('click', function(){
+			document.querySelector('.form-control').value="";
+			document.querySelector("h2").innerHTML="";
+	}); 
 
 	button1.addEventListener('click', function(){
-			if (isNaN(celResult)) {
+		var farInputTemp = parseInt(document.querySelector('.form-control').value);
+		
+			if (isNaN(farInputTemp)) {
 				document.querySelector("h2").innerHTML = "That was a number, like, yesterday. Try again."
 			}
 			else {
-				document.querySelector("h2").innerHTML= "It's " +celResult+ "ºC. I only use Celsius.";	
+				var celResult = ((farInputTemp-32)*(5 / 9)).toFixed(0);
+				document.querySelector("h2").innerHTML= "It's " +celResult+ " &deg;C. I only use Celsius.";	
 			}
-			
-	});
+	}); 
 
-
-}); 
-
-
-
+});
 	// button1.addEventListener ('mouseover', function(){
 	// 		this.style.border="3px solid black";
 	// });
